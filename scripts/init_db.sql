5dd3c1fd0d2b04c80f57c809ccb6e7984fdfcbdb
Mahmoud Taha <mahmouddttahaa@gmail.com>
2026-02-17 20:07:58 +0200

    added db initialization script with bronze, silver and gold schemas

---
+++ scripts/init_db.sql
@@ -0,0 +1,26 @@
+USE MASTER;
+GO
+
+IF DB_ID('DataWarehouse') IS NOT NULL
+BEGIN
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
